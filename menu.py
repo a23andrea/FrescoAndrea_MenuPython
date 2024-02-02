3a0e2b9606d6d6187622d6b10ef08da8f1364fe7
Andrea <a23andreafr@iessanclemente.net>
2024-02-02 12:41:57 +0100

    segundo commit: listo

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import os.path
+import time
 
 
 
@@ -9,7 +10,7 @@ while resp !=6:
     print("Menú, elixe unha opción: ")
     print("1) Crear un directorio")
     print ("2) Comprobar o contido do directorio")
-    print ("3) Cambiar de directorio no que te atopas")
+    print ("3) Calculadora")
     print ("4) Abrir o editor de texto")
     print ("5) Borrar un directorio/arquivo")
     print ("6) Sair")
@@ -20,7 +21,9 @@ while resp !=6:
     os.system('clear')
 
     match resp:
+
         case 1:
+
             print("Escribe o nome do directorio que queres crear:")
             dir1 = input()
 
@@ -35,25 +38,171 @@ while resp !=6:
 
 
         case 2:
+
             print("Contido deste directorio:")
             print(os.listdir())
+
+            print("")
+            print("")
+
+            input("Preme enter para volver ao menu")
+            os.system('clear')
 
 
         case 3:
-            print("Escribe o nome do directorio ao que quere ingresar")
-            print("Contido deste directorio:")
-            print(os.listdir())
-            ruta = input("(Tip: no caso de querer retroceder, introducir '..')")
+
+            print("Que operacion queres realizar?")
+            print("1) Suma")
+            print("2) Resta")
+            print("3) Multiplicación")
+            print("4) División")
             
-            os.system('cd ' + ruta)
+            calc=int(input(""))
+
+            os.system('clear')
+
+            if calc == 1:
+
+                print("Introduce un número enteiro:")
+                num1 = int(input())
+
+                os.system('clear')
+
+                print("Introduce outro número enteiro:")
+                num2 = int(input())
+
+                os.system('clear')
+
+                num3 = num1 + num2
+
+                print(f"{num1} + {num2} = {num3}")
 
 
-        case 2:
-            print("po")
+            if calc == 2:
+
+                print("Introduce un número enteiro:")
+                num1 = int(input())
+
+                os.system('clear')
+
+                print("Introduce outro número enteiro:")
+                num2 = int(input())
+
+                os.system('clear')
+
+                num3 = num1 - num2
+
+                print(f"{num1} - {num2} = {num3}")
+
+
+            if calc == 3:
+
+                print("Introduce un número enteiro:")
+                num1 = int(input())
+
+                os.system('clear')
+
+                print("Introduce outro número enteiro:")
+                num2 = int(input())
+
+                os.system('clear')
+
+                num3 = num1 * num2
+
+                print(f"{num1} * {num2} = {num3}")
+
+
+            if calc == 4:
+
+                print("Introduce un número enteiro:")
+                num1 = int(input())
+
+                os.system('clear')
+
+                print("Introduce outro número enteiro:")
+                num2 = int(input())
+
+                os.system('clear')
+
+
+                if num2 == 0:
+                    
+                    print("Non se pode dividir entre cero")
+
+                else:
+                   
+                    num3 = num1 / num2
+
+                    print(f"{num1} / {num2} = {num3}")
+
+
+            print("")
+            print("")
+
+            input("Preme enter para volver ao menu")
+            os.system('clear')
+
+
+        case 4:
+
+            print("Escribe o nome do arquivo que queres abrir/crear (con extensión):")
+            file = input()
+
+            os.system('nano ' + file)
+
+            os.system('clear')
+
+
+        case 5:
+
+            print("Que tipo de arquivo queres eliminar? (escribe o número)")
+            print("1) directorio")
+            print("2) arquivo")
+            print("3) cambiéi de opinión, quero volver ao menú")
+            option=int(input(""))
+
+            os.system('clear')
+
+            if option == 1:
+
+                print("Contido do directorio:")
+                print(os.listdir())
+                print("Escribe o nome do directorio que queres eliminar (comproba que esté baleiro):")
+                dir2=input("")
+                
+                os.system('clear')
+
+                os.system('rmdir ' + dir2)
+
+                print("O directorio eliminouse correctamente")
+
+
+            if option == 2:
+            
+                print("Contido do directorio:")
+                print(os.listdir())
+                print("Escribe o nome do arquivo que queres eliminar:")
+                arq3=input("")
+                
+                os.system('clear')
+
+                os.system('rm ' + arq3)
+
+                print("O arquivo eliminouse correctamente")
+
+
+
+            input("Preme enter para volver ao menu")
+            os.system('clear')
 
 
         case 6:
             print("")
 
 
-## Notas: os.removedirs(name), os.remove(path, *, dir_fd=None)   
+        case other:
+
+            print("Non existe esa opción, elixe outra do menú")
+            time.sleep(3)
+            os.system('clear')
+
